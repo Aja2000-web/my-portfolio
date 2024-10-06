64a480a659d31a496e2bef1d58188093db854eb0
Aja2000-web <ajaridma123@gmail.com>
2024-10-06 10:36:12 +0530

    Modify component

---
+++ src/components/Ridma.jsx
@@ -0,0 +1,14 @@
+import Ridma from "./components/Ridma";
+import ridma from "./ridma"
+import { getValue } from "@testing-library/user-event/dist/utils";
+
+const ridma = ({value})=> {
+
+    return (
+        <div>
+            <h1>{value}</h1>
+        </div>
+    )
+}
+
+export default App;
