430b068f4a14606343b5273bcd77a73bec78190c
Aja2000-web <ajaridma123@gmail.com>
2024-10-06 10:35:47 +0530

    Create component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-
+import ridma from "./components/ridma";
 
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" >
+      <ridma name={"ridma"}/>
+        </div>
+
+
+      
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+       
+    
   );
 }
 
